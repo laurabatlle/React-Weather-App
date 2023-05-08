d4e719bcb2a86e86cb549c5a0793bedf97111b29
laurabatlle <laura.batlle007@gmail.com>
2023-05-08 20:21:17 +0200

    added Github repository link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app </div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is created by Laura Batlle and it is
+        <a
+          href="https://github.com/laurabatlle/React-Weather-App"
+          target="_blank"
+        >
+          open-sourced in Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
