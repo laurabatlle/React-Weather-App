4d6c8c01a63791aaef615bf67cbe6177ab00dda0
laurabatlle <laura.batlle007@gmail.com>
2023-05-08 20:42:49 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project is created by Laura Batlle and it is
-        <a
-          href="https://github.com/laurabatlle/React-Weather-App"
-          target="_blank"
-        >
-          open-sourced in Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project is created by Laura Batlle and it is
+          <a
+            href="https://github.com/laurabatlle/React-Weather-App"
+            target="_blank"
+          >
+            open-sourced in Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
