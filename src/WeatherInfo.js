10c69245617ef0991d0845991ddfd155f44a4b7c
laurabatlle <laura.batlle007@gmail.com>
2023-05-12 21:31:57 +0200

    Changed background and added unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,36 +1,30 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeatherInfo(props){
-
-    return(
+export default function WeatherInfo(props) {
+  return (
     <div className="weatherInfo">
-    <h1>{props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={props.data.date}/>
-          </li>
-          <li className="text-capitalize">{props.data.description}</li>
-        </ul>
-        <div class="row mt-3">
-          <div class="col-6">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-            />
-            <span className="temperature">{Math.round(props.data.temperature)}</span> 
-            <span className="unit">ºC</span>
-          </div>
-  
-          <div class="col-6">
-            <ul>
-              <li>Humidity:{props.data.humidity} %</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
-          </div>
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div class="row mt-3">
+        <div class="col-6">
+          <img src={props.data.iconUrl} alt={props.data.description} />
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+
+        <div class="col-6">
+          <ul>
+            <li>Humidity:{props.data.humidity} %</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
       </div>
-      
-    );
-
+    </div>
+  );
 }
