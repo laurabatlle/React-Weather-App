1d0b9b439b95af93923c82da40e1cad62ab2c869
laurabatlle <laura.batlle007@gmail.com>
2023-05-13 12:19:38 +0200

    added forecast structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast.js";
 
 export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  const [weatherData, setWeatherData]= useState({ready:false});
-  const [city, setCity]= useState(props.defaultCity);
-  
-  function handleResponse(response){
+  function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
       ready: true,
@@ -16,34 +16,28 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
       city: response.data.city,
-      description: response.data.condition.description, 
+      description: response.data.condition.description,
       date: new Date(response.data.time * 1000),
-      iconUrl:response.data.condition.icon_url   
+      iconUrl: response.data.condition.icon_url,
     });
-  
-    
   }
 
-  function search(){
-    const apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";  
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
-  axios.get(apiUrl).then(handleResponse);
-
+  function search() {
+    const apiKey = "6b72d3t9e0a187fb46324o57dba90ad0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
     search();
-
   }
 
-function handleCityChange(event){
-  setCity(event.target.value);
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-}
-
-  if(weatherData.ready){
-
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <form onSubmit={handleSubmit}>
@@ -57,18 +51,22 @@ function handleCityChange(event){
                 onChange={handleCityChange}
               />
             </div>
-  
+
             <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary w-100" />
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
         </form>
-        <WeatherInfo data={weatherData}/>
-        </div>); }
-  
-  else {
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast data={weatherData} />
+      </div>
+    );
+  } else {
     search();
-   return "Loading..."; } }
-  
-
-  
+    return "Loading...";
+  }
+}
