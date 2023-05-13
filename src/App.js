30f64128b7e2e6d45a4544531e30c92fe8e1aefa
laurabatlle <laura.batlle007@gmail.com>
2023-05-13 11:35:00 +0200

    updated footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <Weather defaultCity="Barcelona" />
       </div>
       <footer>
-        This project is created by Laura Batlle and it is{" "}
+        This project is coded with ❤️ by Laura Batlle and it is{" "}
         <a
           href="https://github.com/laurabatlle/React-Weather-App"
           target="_blank"
