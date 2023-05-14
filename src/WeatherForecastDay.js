c9bfe61e9279ed08881a9077a59a33d86e3604b8
laurabatlle <laura.batlle007@gmail.com>
2023-05-14 16:16:05 +0200

    corrected the forecast for 1 day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,19 +1,34 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let maxTemperature = Math.round(props.data.temperature.maximum);
+    return `${maxTemperature}º`;
+  }
+  function minTemperature() {
+    let minTemperature = Math.round(props.data.temperature.minimum);
+    return `${minTemperature}º`;
+  }
+  function Day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fry", "Sat"];
+    return days[day];
+  }
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.daily.time}</div>
+      <div className="WeatherForecast-day">{Day()}</div>
       <img
-        src={props.data.daily.condition.iconUrl}
-        alt={props.data.daily.condition.description}
+        src={props.data.condition.icon_url}
+        alt={props.data.condition.description}
       />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {props.data.daily.temperature.maximum}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {props.data.daily.temperature.minimum}°
+          {minTemperature()}
         </span>
       </div>
     </div>
